6bf0c3b4cf1c3cb9ac0048b647b731e9c218ec02
Benjamin <hoppeconnect@gmail.com>
2019-01-15 09:30:33 -0500

    all tests passed

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function removeOddSum() {
+function removeOddSum(array, key1, key2) {
+
+result = array.slice(0);
+  for (i = 0 ; i < array.length ; i++) {
+    if ((array[i][key1] + array[i][key2]) % 2 !==0) {
+      result.splice(array.indexOf(result[i]), 1);
+    }
+  }
+  return result
 }
 
 module.exports = removeOddSum;
